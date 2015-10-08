dc11e6061a7e72822c6849a372a1684e8538a474
Charlie Le <chtle@ucdavis.edu>
2015-10-07 18:13:40 -0700

    Removing code that causes failure to launch.

--- DavisCraft/GameViewController.swift
+++ DavisCraft/GameViewController.swift
@@ -9,22 +9,6 @@
 import UIKit
 import SpriteKit
 
-extension SKNode {
-    class func unarchiveFromFile(file : String) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-            
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
-            archiver.finishDecoding()
-            return scene
-        } else {
-            return nil
-        }
-    }
-}
-
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -48,14 +32,6 @@ class GameViewController: UIViewController {
 
     override func shouldAutorotate() -> Bool {
         return true
-    }
-
-    override func supportedInterfaceOrientations() -> Int {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
-        } else {
-            return Int(UIInterfaceOrientationMask.All.rawValue)
-        }
     }
 
     override func didReceiveMemoryWarning() {
