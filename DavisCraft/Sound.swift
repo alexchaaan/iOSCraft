13d1aed03f3df1e259e0663020ccc44d83ee3984
Charlie Le <chtle@ucdavis.edu>
2015-10-18 14:56:54 -0700

    Created sound class for managing music. Moved images to images.xcassets for efficient loading.

---
+++ DavisCraft/Sound.swift
@@ -0,0 +1,40 @@
+//
+//  Sound.swift
+//  DavisCraft
+//
+//  Created by Charlie on 10/18/15.
+//  Copyright © 2015 UCDClassNitta. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+@available(iOS 8.0, *)
+
+class Sound {
+    private static var backgroundMusicPlayer = AVMIDIPlayer()
+    
+    static func playBackgroundMusic(filename: String) {
+        //Load the soundfont
+        let soundFontURL = NSBundle.mainBundle().URLForResource("generalsoundfont",
+            withExtension: "sf2",
+            subdirectory: "snd")
+        
+        //Load the midi file
+        let url = NSBundle.mainBundle().URLForResource(filename,
+            withExtension: nil,
+            subdirectory: "snd/music")
+        guard let midiURL = url else {
+            print("Could not find file: \(filename)")
+            return
+        }
+        
+        do {
+            backgroundMusicPlayer = try AVMIDIPlayer(contentsOfURL: midiURL, soundBankURL:soundFontURL)
+            backgroundMusicPlayer.prepareToPlay()
+            backgroundMusicPlayer.play({playBackgroundMusic(filename)})
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
+}
