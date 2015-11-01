82f52a1d403107b63b0cb6fcc1d4c55826f3cf5c
Javi Perez <fjperez@ucdavis.edu>
2015-10-31 19:18:25 -0700

    Sectioned out the side panel, into description and action sections. Update the view port. Added the menu bar. Fixed select button

--- TileController.swift
+++ TileController.swift
@@ -18,28 +18,54 @@ class Tile: UIViewController{
 
         //MAP
         let a = MapRenderer()
+        //
+        let fullWidth = view.bounds.size.width
+        let fullHeight = view.bounds.size.height
+        
         a.RetrieveFromDat()
         view.backgroundColor = UIColor.blackColor()
         let (_, width, height ) = MapRender().readMap()
         let theMap = MapRender(frame: CGRectMake(0, 0, CGFloat((width-2) * 32), CGFloat(height * 32)))
-        let miniMap = MiniMapRender(frame:CGRectMake(10, 20, view.bounds.size.width / 4 , view.bounds.size.height - 20))
+        let miniMap = MiniMapRender(frame:CGRectMake(1, fullHeight / 20, fullWidth / 4 , fullHeight / 3))
         var battleField: UIScrollView!
         // the size of viewport is the 3/4 of the screen - 1/4 for the side bar panel.
-        battleField = UIScrollView(frame: CGRectMake(view.bounds.size.width / 4, 0, view.bounds.size.width * 3 / 4, view.bounds.size.height))
+        battleField = UIScrollView(frame: CGRectMake(fullWidth / 4, fullHeight / 20, fullWidth * 3 / 4, fullHeight))
         battleField.contentSize = theMap.bounds.size
         battleField.backgroundColor = UIColor.blackColor()
         battleField.scrollRectToVisible(CGRect(x: 2500, y: 1600, width: battleField.frame.width, height: battleField.frame.height), animated: true)
         battleField.addSubview(theMap)
         view.addSubview(battleField)
         
+        let miniHeight = miniMap.bounds.size.height
+        let miniWidth = miniMap.bounds.size.width
         
         //SIDEPANEL
         var sidePanel: UIView!
-        sidePanel = UIView(frame: CGRectMake(0, 0, view.bounds.size.width / 4, view.bounds.size.height))
+        sidePanel = UIView(frame: CGRectMake(0, 0, (fullWidth / 4) + 2, fullHeight))
         sidePanel.backgroundColor = UIColor.blackColor()
         sidePanel.addSubview(miniMap)
         view.addSubview(sidePanel)
         
+        //MENUPANEL by Javi
+        var menuPanel: UIView!
+        menuPanel = UIView(frame: CGRectMake(0, 0, fullWidth, fullHeight / 20))
+        menuPanel.backgroundColor = UIColor.blueColor()
+        //sidePanel.addSubview(miniMap)
+        view.addSubview(menuPanel)
+        
+        //DESCRIPTIONPANEL by Javi
+        var descPanel: UIView!
+        descPanel = UIView(frame: CGRectMake(1, miniHeight + 12, miniWidth, miniHeight/1.1))
+        descPanel.backgroundColor = UIColor.redColor()
+        //sidePanel.addSubview(miniMap)
+        view.addSubview(descPanel)
+        
+        //ACTIONPANEL by Javi
+        var actPanel: UIView!
+        actPanel = UIView(frame: CGRectMake(1, (miniHeight * 2) + 3, miniWidth, miniHeight/1.1))
+        actPanel.backgroundColor = UIColor.yellowColor()
+        //sidePanel.addSubview(miniMap)
+        view.addSubview(actPanel)
         
         //GESTURES
         let tapRec = UITapGestureRecognizer()
